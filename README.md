e369123b9bc4311218b97744050ef249ed15a9fb
Pranab Mir <pranabmir1994@gmail.com>
2025-06-17 00:18:48 +0530

    till data ingestion update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ML_Proj_1
+
+### Workflows of ML-Pipeline
+ 1. Data Ingestion
+ 2. Data Validation
+ 3. Data Transformation
+ 4. Model Trainer
+ 5. Model Evaluation 
+
+
+ ## Workflows
+ 1. Update config.yaml
+ 2. Update schema.yaml
+ 3. Update params.yamp
+ 4. Update entity
+ 5. Update config manager in scr config
+ 6. Update components
+ 7. Update Pipeline
+ 8. Update main.py
--- src/datascience/utils/common.py
+++ src/datascience/utils/common.py
@@ -11,18 +11,18 @@ from box.exceptions import BoxValueError
 
 
 @ensure_annotations
-def read_yaml(path_to_yaml: Path)->ConfigBox:
-    """
-    reads yaml file and returns
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
 
     Args:
         path_to_yaml (str): path like input
+
     Raises:
         ValueError: if yaml file is empty
-        e:empty file
+        e: empty file
+
     Returns:
-        ConfigBox: ConfigBox type   
-    
+        ConfigBox: ConfigBox type
     """
     try:
         with open(path_to_yaml) as yaml_file:
@@ -33,21 +33,34 @@ def read_yaml(path_to_yaml: Path)->ConfigBox:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
+        
 
 
 @ensure_annotations
-def save_json(path: Path,data:dict) ->ConfigBox:
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
-    save json data
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """save json data
 
     Args:
         path (Path): path to json file
         data (dict): data to be saved in json file
     """
-    with open(path,"w") as f:
-        json.dump(data,f,indent = 4)
-    logger.info(f'json file saved at: {path}')
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
 
+    logger.info(f"json file saved at: {path}")
 
 @ensure_annotations
 def load_json(path: Path) -> ConfigBox:
